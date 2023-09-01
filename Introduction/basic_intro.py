6429617c26fd515e53c4f615e62e2e7eabb6d4f3
programmingChiira <programmingchiira@gmail.com>
2023-09-01 10:31:11 +0300

    basic intro dot py

---
+++ Introduction/basic_intro.py
@@ -0,0 +1,18 @@
+#basic print
+print("Kenya")
+
+#basic arithimetic
+print(10+50)
+
+#varible use and printing
+x = 7
+print(x)
+print(type(x))
+
+#working with inputs and arithimetic
+x = int(input("Enter numerical value for X: "))
+y = int(input("Enter numerical value for Y: "))
+print("You have inputed the value for X as:", (x) , " and the value for Y as: ", (y))
+print("The product for the two inputs is ", (x*y))
+print("The sum for the two inputs is ", (x+y))
+print("The subtraction for the two inputs is ", (x-y))
