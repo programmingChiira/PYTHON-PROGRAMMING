b853b8d5f7355d0191b4061c2167639a3ccf1021
programmingChiira <programmingchiira@gmail.com>
2023-09-11 08:45:19 +0300

    discard item

---
+++ PythonSets/RemoveSet/discard_item.py
@@ -0,0 +1,8 @@
+# Remove "banana" by using the discard() method:
+# Note: If the item to remove does not exist, discard() will NOT raise an error.
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.discard("banana")
+
+print(thisset) 
