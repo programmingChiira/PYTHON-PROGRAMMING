06f5018fed8bc252c3e888552c1b10745d9cf939
programmingChiira <programmingchiira@gmail.com>
2023-09-11 08:43:42 +0300

    remite set item

---
+++ PythonSets/RemoveSet/remove_item.py
@@ -0,0 +1,8 @@
+# Remove "banana" by using the remove() method:
+# Note: If the item to remove does not exist, remove() will raise an error.
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.remove("banana")
+
+print(thisset) 
