c0346bbdedd81c7300a47e34f6fba19fcc2d8d0d
programmingChiira <programmingchiira@gmail.com>
2023-09-11 08:35:08 +0300

    To add items from another set into the current set, use the update() method

---
+++ PythonSets/AddItems/add_items_from_one_set_to_another+set.py
@@ -0,0 +1,9 @@
+# To add items from another set into the current set, use the update() method.
+
+# Add elements from tropical into thisset:
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset) 
