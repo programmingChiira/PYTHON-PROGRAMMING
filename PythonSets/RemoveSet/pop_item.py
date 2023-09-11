d966793496acdeb90d28cb32aaf9b977f4baaca7
programmingChiira <programmingchiira@gmail.com>
2023-09-11 08:46:36 +0300

    pop() set items

---
+++ PythonSets/RemoveSet/pop_item.py
@@ -0,0 +1,9 @@
+# Remove a random item by using the pop() method:
+# Note: Sets are unordered, so when using the pop() method, you do not know which item that gets removed. 
+thisset = {"apple", "banana", "cherry"}
+
+x = thisset.pop()
+
+print(x)
+
+print(thisset) 
