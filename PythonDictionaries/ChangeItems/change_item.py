89e27e4ba1a9b16d426e51682e708c3f96e0e1fc
programmingChiira <programmingchiira@gmail.com>
2023-09-12 11:35:30 +0300

    You can change the value of a specific item by referring to its key name

---
+++ PythonDictionaries/ChangeItems/change_item.py
@@ -0,0 +1,10 @@
+# You can change the value of a specific item by referring to its key name
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+thisdict["year"] = 2018
+
+print(thisdict)
