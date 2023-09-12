825363ebcd8f62450d36784181addad15ce29786
programmingChiira <programmingchiira@gmail.com>
2023-09-12 11:21:37 +0300

    Get dictionary values using value() method

---
+++ PythonDictionaries/AccessItems/get_dictionary_value.py
@@ -0,0 +1,10 @@
+# The values() method will return a list of all the values in the dictionary.
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = thisdict.values()
+
+print(x)
