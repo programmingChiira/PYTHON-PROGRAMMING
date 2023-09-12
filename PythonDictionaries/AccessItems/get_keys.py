b69d5ff0a497be2b49256e6ebf10777cb962dbd7
programmingChiira <programmingchiira@gmail.com>
2023-09-12 11:13:23 +0300

    Using keys() method to access keys in the dictionary

---
+++ PythonDictionaries/AccessItems/get_keys.py
@@ -0,0 +1,10 @@
+# The keys() method will return a list of all the keys in the dictionary.
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = thisdict.keys()
+
+print(x)
