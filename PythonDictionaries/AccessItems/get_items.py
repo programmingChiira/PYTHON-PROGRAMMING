fdb713e33462b52404916239ba25496440c7b39c
programmingChiira <programmingchiira@gmail.com>
2023-09-12 11:26:37 +0300

    Get both key and values using items() method

---
+++ PythonDictionaries/AccessItems/get_items.py
@@ -0,0 +1,11 @@
+# The items() method will return each item in a dictionary, as tuples in a list.
+# Get a list of the key:value pairs
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = thisdict.items()
+
+print(x)
