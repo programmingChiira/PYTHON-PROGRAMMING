4a7eb079d3d4d97d94edb9f409606ff189825b3c
programmingChiira <programmingchiira@gmail.com>
2023-09-13 13:16:15 +0300

    Using the Elif keyword with the id condition

---
+++ PythonIfElse/elif_keyword.py
@@ -0,0 +1,8 @@
+# The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition".
+
+a = 33
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
