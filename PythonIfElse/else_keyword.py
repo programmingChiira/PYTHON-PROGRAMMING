d11ef0413e7fb417e647e39f28f65863301fa5e3
programmingChiira <programmingchiira@gmail.com>
2023-09-14 12:02:40 +0300

    The else keyword catches anything which isn't caught by the preceding conditions

---
+++ PythonIfElse/else_keyword.py
@@ -0,0 +1,10 @@
+# The else keyword catches anything which isn't caught by the preceding conditions.
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
