48ed3e2ec09586333d17f0e2dde020f3b14b1bb9
programmingChiira <programmingchiira@gmail.com>
2023-09-15 19:27:42 +0300

    The else statement can also be used in a while lop

---
+++ PythonWhileLoops/the_else_statement.py
@@ -0,0 +1,9 @@
+# With the else statement we can run a block of code once when the condition no longer is true:
+
+# Print a message once the condition is false:
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
