6b6d6f2cbeb28a303ffc209c0a05a2bd801026b5
programmingChiira <programmingchiira@gmail.com>
2023-09-15 19:22:41 +0300

    Using the vreak statement in a while loop

---
+++ PythonWhileLoops/the_break_statement.py
@@ -0,0 +1,9 @@
+# With the break statement we can stop the loop even if the while condition is true:
+
+# Exit the loop when i is 3:
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1 
