c76db337c71e692f413a70f096320f1eada29e0f
programmingChiira <programmingchiira@gmail.com>
2023-09-25 22:04:52 +0300

    using key and value in functions

---
+++ PythonFunctions/keyword_arguments.py
@@ -0,0 +1,8 @@
+# You can also send arguments with the key = value syntax.
+
+def my_function(sibling3, sibling2, sibling1):
+    print("The youngest sibling is " + sibling3)
+    print("The middle sibling is " + sibling2)
+    print("The oldest sibling is " + sibling1)
+
+my_function(sibling1 = "Joseph", sibling2 = "Dennis", sibling3 = "Magdalene") 
