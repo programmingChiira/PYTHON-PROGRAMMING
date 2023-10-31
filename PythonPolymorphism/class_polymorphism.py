d2d3e1f78ed5b617ef08885d0b9761ee91d5b18f
programmingChiira <programmingchiira@gmail.com>
2023-10-31 10:23:30 +0300

    class polymorphism

---
+++ PythonPolymorphism/class_polymorphism.py
@@ -0,0 +1,34 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("Drive!")
+
+class Boat:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("Sail!")
+
+class Plane:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("Fly!")
+
+car1 = Car("Ford", "Mustang")
+boat1 = Boat("Ibiza", "Touring 20")
+plane1 = Plane("Boeing", "747")
+
+print(car1.brand, car1.model)
+print(boat1.brand, boat1.model)
+print(plane1.brand, plane1.model)
+
+for x in (car1, boat1, plane1):
+    x.move()
