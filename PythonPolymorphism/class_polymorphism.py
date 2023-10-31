1ee92aad18d906d64262af5d603b081c3131869a
programmingChiira <programmingchiira@gmail.com>
2023-10-31 10:23:48 +0300

    changes in class polymorphism

--- PythonPolymorphism/class_polymorphism.py
+++ PythonPolymorphism/class_polymorphism.py
@@ -26,9 +26,5 @@ car1 = Car("Ford", "Mustang")
 boat1 = Boat("Ibiza", "Touring 20")
 plane1 = Plane("Boeing", "747")
 
-print(car1.brand, car1.model)
-print(boat1.brand, boat1.model)
-print(plane1.brand, plane1.model)
-
 for x in (car1, boat1, plane1):
     x.move()
